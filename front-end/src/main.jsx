5be5f5f45756bbdcb11b9ce372bf92448c6bc602
Kevin04C <kevincespedes2004@gmai.com>
2022-09-23 11:38:05 -0500

    añadiendo el store y las rutas para la app

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { Provider } from 'react-redux';
+import { EcommerApp } from './EcommerApp';
 import './index.css'
+import store from './store/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <EcommerApp />
+    </Provider>
   </React.StrictMode>
 )
