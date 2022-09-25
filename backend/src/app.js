f243c8b760dc4638244eb3e81f12cf465ef30b5b
franz <gavino_10@hotmail.es>
2022-09-25 16:46:42 -0500

    trabajando backend1

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,21 +1,30 @@
 //bienvenidos a el ApiRestFull del ecommerce desarrollado en react
-const express = require("express");
-const cors = require("cors");
-const mysql = require("mysql");
+const express= require('express');
+const mysql=require('mysql');
+const cors=require('cors');
 
-const app = express();
+const app= express();
 app.use(express.json());
 app.use(cors());
 
-var connection = {
-  host: "",
-  user: "",
-  password: "",
-  database: "",
-};
+const connection=({
+  host:'us-cdbr-east-06.cleardb.net',
+  user:'bd81d39baf9f5d',
+  password:'f1970e88',
+  database:'heroku_e70b6afe379c951',
+});
+//primaformadeconectarmeala bd
+/* connection.connect(function(error){
+  if(error){
+    throw error;
+
+  }else{
+    console.log("conexion exitosa a la base de datos");
+  }
+}) */
 
 //para evitar futuras desconeccioness (inicio)
-var conexion;
+let conexion;
 function handleDisconnect() {
   conexion = mysql.createConnection(connection);
   conexion.connect(function (err) {
@@ -37,11 +46,103 @@ handleDisconnect();
 
 //(finalll)
 
+
 app.get("/", function (req, res) {
   res.send("ruta INICIO");
 });
+//REGISTRO CRUD------------------------------------------------------------------------------------------------------>
+//ver tablas registro
+app.get('/api/registroUsuario', (req,res)=>{
+  conexion.query('SELECT * FROM tb_usuario', (error,filas)=>{
+      if(error){
+          throw error;
 
-const PORT = process.env.PORT || 3000;
+      }else{
+          res.send(filas);
+      }
+  })
+});
+
+
+//-------------------------------------------------------------------------------------------------------------------->
+//ROL CRUD
+//ver tablas rol
+
+app.get('/api/rol', (req,res)=>{
+  conexion.query('SELECT * FROM rol', (error,filas)=>{
+      if(error){
+          throw error;
+
+      }else{
+          res.send(filas);
+      }
+  })
+});
+
+//mostrar solo un ROL
+app.get('/api/rol/:id', (req,res)=>{
+  conexion.query('SELECT * FROM rol WHERE idRol=?',[req.params.id], (error,fila)=>{
+      if(error){
+          throw error;
+
+      }else{
+          res.send(fila);
+      }
+  })
+});
+
+//para insertar
+app.post('/api/rol',(req,res)=>{
+  let idRol =req.params.idRol;
+  let tipoRol=req.body.tipoRol;
+  let data=[tipoRol,idRol]
+  /* let sql="INSERT INTO rol SET ?"; */
+  let sql="INSERT INTO `rol` (`idRol`, `tipoRol`) VALUES ('?', '?')"
+  conexion.query(sql, data, function(error, results){
+      if(error){
+          throw error;
+
+      }else{
+          Object.assign(data,{id:results.insertId});
+          res.send(data);
+          
+      }
+  });
+});
+
+
+//para editar
+app.put('/api/rol/:id', (req,res)=>{
+  let idRol =req.params.idRol;
+  let tipoRol=req.body.tipoRol;
+  let sql="UPDATE rol SET tipoRol=? WHERE idRol=?";
+  conexion.query(sql,[tipoRol,idRol], function(error,results){
+      if(error){
+          throw error;
+
+      }else{
+          res.send(results);
+          
+      }
+  });
+});
+
+//eliminar articulos
+app.delete('/api/rol/:id', (req,res)=>{
+  conexion.query('DELETE FROM rol WHERE idRol=?', [req.params.id],function(error,filas){
+      if(error){
+          throw error;
+
+      }else{
+          res.send(filas);
+          
+      }
+  });
+});
+
+
+//conexion de puertos------------------------------------------------------------------------------------------->
+var PORT = process.env.PORT || 3000;
 
 app.listen(PORT, function () {
   console.log("servidor ok en puerto:" + PORT);
